b81873b1255133d80ade5f84da294a1af28f53d0
GB <1106205841@qq.com>
2024-08-01 00:13:08 +0800

    Single Cell

--- pouch_cells/DataFetching.py
+++ pouch_cells/DataFetching.py
@@ -1,15 +1,14 @@
 # APAC
-
 import NewareNDA
+import numpy as np
 
-df = NewareNDA.read('LHB.ndax')
+df = NewareNDA.read('SDM`101_127.0.0.1-BTS83-46-7-5-655.ndax')
 print(df)
 
 # save df into csv:
-import numpy as np
 
 # Open the file in append mode
-with open("output.csv", "a") as csv_file:
+with open("SingleCell.csv", "a") as csv_file:
     # Create a NumPy array with the new data
     new_data = np.array(df)
     # Append the data to the file
